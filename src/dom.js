2dd308a051426aef23f66fb211149469a6f778ee
Ian Lucas <ianlucas_ferreira@hotmail.com>
2019-05-05 03:26:39 -0300

    Add draw method to handle cursor

--- src/dom.js
+++ src/dom.js
@@ -1,21 +1,33 @@
 export default function ui() {
-  const input = MathJax.HTML.Element('div', { class: 'mje-input' })
+  const input = MathJax.HTML.Element('div', { className: 'mje-input' })
+  const cursor = MathJax.HTML.Element('div', { className: 'mje-cursor' })
   let jax = null
+  
+  document.body.appendChild(cursor)
 
   return {
+    input() {
+      return input
+    },
+
     value(newValue) {
       return new Promise(resolve => {
         if (jax) {
           return jax.Text(newValue, resolve)
         }
         input.innerHTML = newValue
-        MathJax.Hub.Queue(['Typeset', MathJax.Hub, node, () => {
-          jax = MathJax.Hub.getAllJax(node)[0]
+        MathJax.Hub.Queue(['Typeset', MathJax.Hub, input, () => {
+          jax = MathJax.Hub.getAllJax(input)[0]
           resolve()
         }])
       })
     },
 
-
+    draw(data) {
+      console.log(data)
+      cursor.style.left = `${data.x}px`
+      cursor.style.top = `${data.y}px`
+      cursor.style.height = `${data.height}px`
+    }
   }
 }
