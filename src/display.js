e93d7909131259fc5d4df97068e0dc36eac84a86
Ian Lucas <ianlucas_ferreira@hotmail.com>
2019-05-05 03:26:11 -0300

    Should return outerHtml of displayed math

--- src/display.js
+++ src/display.js
@@ -4,12 +4,12 @@ export default function display(math) {
   const displayed = math.cloneNode(true)
   each(displayed, source => {
     switch (source.tagName) {
-    case 'mrow':
-    case 'math':
+    case 'MROW':
+    case 'MATH':
       if (!source.children.length) {
         source.appendChild(MathJax.HTML.Element('mi', {}, ['?']))
       }
     }
   })
-  return displayed
+  return displayed.outerHTML
 }
