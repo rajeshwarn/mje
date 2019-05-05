f470d28f60fdfd9e91e643813c80cd02639194ad
Ian Lucas <ianlucas_ferreira@hotmail.com>
2019-05-05 03:28:58 -0300

    Use editor in test page

--- test/Main.vue
+++ test/Main.vue
@@ -1,28 +1,34 @@
 <template>
   <div>
-    <math>
-      <msqrt>
-        <mrow>
-          <mn>2</mn>
-        </mrow>
-      </msqrt>
-      <mo>=</mo>
-      <mn>1</mn>
-      <mo>.</mo>
-      <mn>4</mn>
-    </math>
+    <h1>MathJax Editor Neo</h1>
+    <div id="target"></div>
   </div>
 </template>
 
 <script>
+  import mjs from '../src/index'
+
   export default {
-    created() {
+    mounted() {
       const mathJaxScript = document.createElement('script')
       mathJaxScript.src = 'https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.5/MathJax.js?config=TeX-MML-AM_CHTML'
       mathJaxScript.onload = () => {
-        console.log('opa!')
+        const target = document.getElementById('target')
+        window.editor = mjs(target)
       }
       document.body.appendChild(mathJaxScript)
     }
   }
 </script>
+
+<style>
+  .mje-input {
+    padding: 1em;
+  }
+
+  .mje-cursor {
+    background-color: #000;
+    position: absolute;
+    width: 1px;
+  }
+</style>
