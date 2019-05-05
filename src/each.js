1fbd3148e11e4eeca7f3b3eac2bc27942af50faf
Ian Lucas <ianlucas_ferreira@hotmail.com>
2019-05-05 03:27:08 -0300

    Update each logic to define the navigation path

--- src/each.js
+++ src/each.js
@@ -1,12 +1,17 @@
 export default function each(source, handler) {
   const walk = el => {
+    if (el.tagName !== 'MROW' && el.tagName !== 'MATH') {
+      handler(el)
+    }
     if (el.children) {
       const len = el.children.length
       for (let i = 0; i < len; i++) {
         walk(el.children[i])
       }
     }
-    handler(el)
+    if (el.tagName === 'MROW' || el.tagName === 'MATH') {
+      handler(el)
+    }
   }
   walk(source)
 }
