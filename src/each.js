239a7de80397281cd3991873f2cec3c423de36de
Ian Lucas <ianlucas_ferreira@hotmail.com>
2019-05-07 02:38:13 -0300

    Cleaning and documenting

--- src/each.js
+++ src/each.js
@@ -1,17 +1,30 @@
+/**
+ * Walk through each element within a math element.
+ * @param {HTMLElement} source 
+ * @param {Function} handler 
+ */
 export default function each(source, handler) {
   const walk = el => {
-    if (el.tagName !== 'MROW' && el.tagName !== 'MATH') {
+    const isContainer = (
+      el.tagName === 'MROW'
+      || el.tagName === 'MATH'
+    )
+
+    if (!isContainer) {
       handler(el)
     }
+
     if (el.children) {
       const len = el.children.length
       for (let i = 0; i < len; i++) {
         walk(el.children[i])
       }
     }
-    if (el.tagName === 'MROW' || el.tagName === 'MATH') {
+
+    if (isContainer) {
       handler(el)
     }
   }
+
   walk(source)
 }
