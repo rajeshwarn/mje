b92c99834db50704f53d74fa80da9178fbcce486
Ian Lucas <ianlucas_ferreira@hotmail.com>
2019-05-07 02:23:00 -0300

    Add basic documentation

--- src/view.js
+++ src/view.js
@@ -1,18 +1,28 @@
 export default function view() {
+  /** @type {HTMLElement} */
   const container = MathJax.HTML.Element('div', { className: 'mje-container' })
+  /** @type {HTMLElement} */
   const cursor = MathJax.HTML.Element('div', { className: 'mje-cursor' })
   /** @type {HTMLElement} */
   const input = MathJax.HTML.Element('input', { className: 'mje-input' })
 
+  /** @type {Object} MathJax's Jax element. */
   let jax = null
+  /** @type {Boolean} Focus state of the controller. */
   let focused = false
+  /** @type {Boolean} Hover state of the controller */
   let hover = false
+  /** @type {Object} Event handlers of the controller. */
   let events = {
     click: null,
     input: null,
     code: null
   }
   
+  /**
+   * Update the user interface.
+   * @return {Void}
+   */
   const update = () => {
     if (focused) {
       container.classList.add('focused')
@@ -22,6 +32,10 @@ export default function view() {
     }
   }
 
+  /**
+   * Handle outside UI bounds clicks.
+   * @return {Void}
+   */
   const handleOutsideClick = () => {
     console.log(hover)
     if (!hover) {
@@ -31,6 +45,11 @@ export default function view() {
     }
   }
 
+  /**
+   * Handle inside UI bounds clicks.
+   * @param {MouseEvent} e 
+   * @return {Void}
+   */
   const handleInsideClick = e => {
     focused = true
     events.click(e.clientX, e.clientY)
@@ -38,14 +57,26 @@ export default function view() {
     update()
   }
 
+  /**
+   * Handle UI mouseenter event.
+   * @return {Void}
+   */
   const handleMouseEnter = () => {
     hover = true
   }
 
+  /**
+   * Handle UI mouseleave event.
+   * @return {Void}
+   */
   const handleMouseLeave = () => {
     hover = false
   }
 
+  /**
+   * Handle input blur event.
+   * @return {Void}
+   */
   const handleBlur = () => {
     if (!hover) {
       focused = false
@@ -55,12 +86,21 @@ export default function view() {
     return update()
   }
 
+  /**
+   * Handle input event.
+   * @return {Void}
+   */
   const handleInput = () => {
     if (!input.value.length) {return}
     events.input(input.value)
     input.value = ''
   }
 
+  /**
+   * Handle input keydown event.
+   * @param {KeyboardEvent} e
+   * @return {Void}
+   */
   const handleKeydown = e => {
     events.code(e.keyCode)
   }
@@ -80,10 +120,18 @@ export default function view() {
   return {
     events,
 
+    /**
+     * Get the main container.
+     * @return {HTMLElement}
+     */
     container() {
       return container
     },
 
+    /**
+     * Set display mathematics.
+     * @param {HTMLElement} val 
+     */
     value(val) {
       return new Promise(resolve => {
         if (jax) {
@@ -97,6 +145,10 @@ export default function view() {
       })
     },
 
+    /**
+     * Draw cursor.
+     * @param {Object} data 
+     */
     draw(data) {
       console.log(data)
       cursor.style.left = `${data.x}px`
