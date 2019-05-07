3237cf3f9b1166a3369fc45d172d68c112282298
Ian Lucas <ianlucas_ferreira@hotmail.com>
2019-05-07 02:34:48 -0300

    Cleaning and documenting

--- src/recalculate.js
+++ src/recalculate.js
@@ -1,40 +1,48 @@
 import each from './each'
 
+/**
+ * Get the viewport coordinates of draw mathematics by MathJax.
+ * This will then be used to draw the cursor.
+ * @param {HTMLElement} math 
+ */
 export default function recalculate(math) {
+  /** @type {Array} */
   const path = []
+  /** @type {HTMLElement} */
   let previous = null
+  /** @type {Object} */
+  let prevData = null
+
   each(math, source => {
     const element = document.getElementById(source.id)
-    const rect = element.getBoundingClientRect();
+    const rect = element.getBoundingClientRect()
+    const data = {
+      height: rect.height,
+      source,
+      previous
+    }
 
-    if (path.length) {
-      path[path.length - 1].next = source
+    if (prevData) {
+      prevData.next = source
     }
 
     switch (source.tagName) {
     case 'MATH':
     case 'MROW':
-      path.push({
-        x: rect.width + rect.left,
-        y: rect.top,
-        height: rect.height,
-        source,
-        previous
-      })
-      previous = source
-      return
+      data.x = rect.width + rect.left
+      data.y = rect.top
+      break
 
     default:
-      path.push({
-        x: rect.left,
-        y: rect.top,
-        height: rect.height,
-        source,
-        previous
-      })
-      previous = source
-      return
+      data.x = rect.left
+      data.y = rect.top
+      break
     }
+
+    path.push(data)
+    previous = source
+    prevData = data
   })
+  
   return path
 }
